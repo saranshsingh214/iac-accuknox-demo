377f591e578792dc7893b737108304d67b3e9cec
Saransh Singh <111219765+saranshsingh214@users.noreply.github.com>
2026-01-19 13:42:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-accuknox-demo-bucket-12345"
-  acl    = "public-read"
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-} 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket-214"
+}
